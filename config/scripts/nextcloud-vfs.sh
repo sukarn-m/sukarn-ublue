912d79ea027dd8c924098bb5de7f85aa185616ff
Sukarn <10946339+sukarn-m@users.noreply.github.com>
2023-12-10 09:22:03 +0530

    Update nextcloud-vfs.sh

--- config/scripts/nextcloud-vfs.sh
+++ config/scripts/nextcloud-vfs.sh
@@ -4,5 +4,5 @@
 set -oue pipefail
 
 echo "Linking nextcloud libraries to enable virtual files support."
-ln -s -v /usr/lib64//usr/lib64/nextcloudsync_vfs_suffix.so /usr/lib64/qt5/plugins/nextcloudsync_vfs_suffix.so
+ln -s -v /usr/lib64/nextcloudsync_vfs_suffix.so /usr/lib64/qt5/plugins/nextcloudsync_vfs_suffix.so
 ln -s -v /usr/lib64/nextcloudsync_vfs_xattr.so /usr/lib64/qt5/plugins/nextcloudsync_vfs_xattr.so
