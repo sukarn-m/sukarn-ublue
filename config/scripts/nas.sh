6a10d519ab76003cdc477345e98028cc6b6211a5
Sukarn <10946339+sukarn-m@users.noreply.github.com>
2023-12-14 00:12:22 +0530

    Update nas.sh

--- config/scripts/nas.sh
+++ config/scripts/nas.sh
@@ -5,12 +5,9 @@ set -oue pipefail
 
 # Set permissions for certificate generation script.
 chmod 0700 /usr/bin/generate-ssh-certificate.sh
-chcon -u system_u -t bin_t /usr/bin/generate-ssh-certificate.sh
 
 # Set permissions for nas mountpoints.
 chmod 0644 /usr/lib/systemd/system/var-mnt-nas.mount /usr/lib/systemd/system/var-mnt-nas.automount
-chcon -u system_u -t systemd_unit_file_t /usr/lib/systemd/system/var-mnt-nas.mount
-chcon -u system_u -t systemd_unit_file_t /usr/lib/systemd/system/var-mnt-nas.automount
 
 # Enable everything
 systemctl daemon-reload
