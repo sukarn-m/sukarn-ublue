626e13f47766b371184418366d93e1d79faa15f7
sukarn-m <10946339+sukarn-m@users.noreply.github.com>
2023-12-17 13:03:07 +0530

    Logo updates

--- config/scripts/bazzite-nvidia.sh
+++ config/scripts/bazzite-nvidia.sh
@@ -3,6 +3,12 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
+# Nvidia specific stuff
+
 file_path="/usr/bin/bazzite-hardware-setup"
 
 sed -i "s,^IMAGE_FLAVOR=.*,IMAGE_FLAVOR=nvidia," ${file_path}
+
+# Logo / branding stuff
+
+rm -v /usr/share/pixmaps/fedora_whitelogo.svg
