6b52ad0898bd4e75a5d0be545514628bb8be28fd
Sukarn <10946339+sukarn-m@users.noreply.github.com>
2024-01-02 18:57:41 +0530

    Update bazzite-nvidia.sh

--- config/scripts/bazzite-nvidia.sh
+++ config/scripts/bazzite-nvidia.sh
@@ -4,11 +4,12 @@
 set -oue pipefail
 
 # Nvidia specific stuff
-
 file_path="/usr/bin/bazzite-hardware-setup"
-
 sed -i "s,^IMAGE_FLAVOR=.*,IMAGE_FLAVOR=nvidia," ${file_path}
 
-# Logo / branding stuff
+# Nvidia specific Gnome flickering / tearing fix
+echo "# Gnome flickering / tearing fix" >> /usr/etc/environment
+echo "MUTTER_DEBUG_FORCE_EGL_STREAM=1" >> /usr/etc/environment
 
+# Logo / branding stuff
 rm -v /usr/share/pixmaps/fedora_whitelogo.svg
