d8fcbaa72c43688fa18d11f7b5a2d3ae35c9c39a
Sukarn <10946339+sukarn-m@users.noreply.github.com>
2024-01-03 17:02:03 +0530

    Switch desktop to silverblue-nvidia

--- config/scripts/desktop-nvidia.sh
+++ config/scripts/desktop-nvidia.sh
@@ -3,13 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-# Nvidia specific stuff
-file_path="/usr/bin/bazzite-hardware-setup"
-sed -i "s,^IMAGE_FLAVOR=.*,IMAGE_FLAVOR=nvidia," ${file_path}
-
 # Nvidia specific Gnome flickering / tearing fix
 echo "# Gnome flickering / tearing fix" >> /usr/etc/environment
 echo "MUTTER_DEBUG_FORCE_EGL_STREAM=1" >> /usr/etc/environment
-
-# Logo / branding stuff
-rm -v /usr/share/pixmaps/fedora_whitelogo.svg
