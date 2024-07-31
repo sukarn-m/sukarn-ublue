d15dbc5249050083626a55b2d6b15d23997ab55c
sukarn-m <10946339+sukarn-m@users.noreply.github.com>
2024-07-31 12:17:28 +0530

    coreos_kernel.sh: dynamically check on kojipkgs.

--- files/scripts/coreos_kernel.sh
+++ files/scripts/coreos_kernel.sh
@@ -3,22 +3,21 @@
 ## Based on:
 ## https://github.com/ublue-os/bluefin/blob/da83af82256e35e84e51e93de3958ac1eb9d8de6/scripts/build-image.sh#L24
 
-set -eoux pipefail
+set -eou pipefail
 
 coreos_kernel_release=$(skopeo inspect docker://quay.io/fedora/fedora-coreos:stable | jq -r '.Labels["ostree.linux"] | split(".x86_64")[0]')
 major_minor_patch=$(echo "$coreos_kernel_release" | cut -d '-' -f 1)
 latest_fedora_version=$(echo "$coreos_kernel_release" | grep -oP 'fc\K[0-9]+')
 running_fedora_version=$(grep -Po "(?<=VERSION_ID=)\d+" /usr/lib/os-release)
-if [ $running_fedora_version -eq $latest_fedora_version ]; then
-    coreos_kernel_subnum=300
-elif [ $running_fedora_version -eq $(($latest_fedora_version - 1)) ]; then
-    coreos_kernel_subnum=200
-elif [ $running_fedora_version -eq $(($latest_fedora_version - 2)) ]; then
-    coreos_kernel_subnum=100
-else
-    echo "Fedora version mismatch in coreos_kernel.sh"
-    exit 1
-fi
+
+dir_names=$(curl -sS https://kojipkgs.fedoraproject.org/packages/kernel/${major_minor_patch}/ 2>&1 | grep '<a href=' | sed 's|^<a href="\([^"]*\)">.*|\1|')
+for dir_name in $dir_names; do
+    if [[ $dir_name =~ fc${running_fedora_version} ]]; then
+        coreos_kernel_subnum=$(echo $dir_name | grep -oE '[0-9]{3}' | head -1)
+        break
+    fi
+done
+
 KERNEL_VERSION="${major_minor_patch}-${coreos_kernel_subnum}.fc$(("$running_fedora_version"))"
 KERNEL_MAJOR_MINOR_PATCH=$(echo "$KERNEL_VERSION" | cut -d '-' -f 1)
 KERNEL_RELEASE=$(echo "$KERNEL_VERSION" | cut -d '-' -f 2)
