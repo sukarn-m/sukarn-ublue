cf156c562c59ae94f8b527e5f79039b92b6295ff
sukarn-m <10946339+sukarn-m@users.noreply.github.com>
2024-08-07 23:53:02 +0530

    Multiple changes: modified:   .github/workflows/build-gnome.yml modified:   files/scripts/ublue-rpms.sh new file:   files/scripts/ublue-update.sh new file:   recipes/base-gnome.yml new file:   recipes/base-ublue.yml modified:   recipes/image-budgie.yml modified:   recipes/variant-gnome.yml

--- files/scripts/ublue-rpms.sh
+++ files/scripts/ublue-rpms.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-rpm-ostree install /tmp/rpms/*.noarch.rpm
+rpm-ostree install /tmp/config-rpms/*.noarch.rpm
---
+++ files/scripts/ublue-update.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+
+get_config_value() {
+    sed -n '/^'"$1"'=/{s/'"$1"'=//;p}' "$2"
+}
+
+set_config_value() {
+    CURRENT=$(get_config_value "$1" "$3")
+    sed -i 's/'"$1"'='"$CURRENT"'/'"$1"'='"$2"'/g' "$3"
+}
+
+# Check if ublue-os-update-services rpm is installed, these services conflict with ublue-update
+if rpm -q ublue-os-update-services > /dev/null; then
+    rpm-ostree override remove ublue-os-update-services
+fi
+
+# Change the conflicting update policy for rpm-ostreed
+RPM_OSTREE_CONFIG="/etc/rpm-ostreed.conf"
+
+if [[ -f "$RPM_OSTREE_CONFIG" ]]; then
+    if [[ $(get_config_value "AutomaticUpdatePolicy" "$RPM_OSTREE_CONFIG") == "stage" ]]; then
+        set_config_value "AutomaticUpdatePolicy" "none" "$RPM_OSTREE_CONFIG"
+    fi
+fi
+systemctl disable rpm-ostreed-automatic.timer
+
+# Fetch ublue COPR
+REPO="https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-${OS_VERSION}/ublue-os-staging-fedora-${OS_VERSION}.repo"
+wget -O "/etc/yum.repos.d/ublue-os-staging-fedora-${OS_VERSION}.repo" "${REPO//[$'\t\r\n ']}"
+
+# topgrade is REQUIRED by ublue-update to install
+rpm-ostree install topgrade
+rpm-ostree install /tmp/update-rpm/ublue-update.noarch.rpm
+
+# Remove ublue COPR
+rm /etc/yum.repos.d/ublue-os-staging-fedora-*.repo
