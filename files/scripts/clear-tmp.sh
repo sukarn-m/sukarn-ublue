d42acefbd202714cb5b9143ad00dfe42a3357dcc
sukarn-m <10946339+sukarn-m@users.noreply.github.com>
2024-08-08 22:00:03 +0530

    modified:   files/scripts/clear-tmp.sh

--- files/scripts/clear-tmp.sh
+++ files/scripts/clear-tmp.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-rm -rf /tmp/*
+set -oeu pipefail
+
+if [ -d /tmp/rpm-repos ]; then
+  rm -rfv /tmp/rpm-repos
+fi
+
+if [ -d /tmp/kernel-rpms ]; then
+  rm -rfv /tmp/kernel-rpms
+fi
+
+if [-d /tmp/config-rpms ]; then
+  rm -rfv /tmp/config-rpms
+fi
