0fb6e5274e5722e4d54c55f375b703b74dbc5c59
sukarn-m <10946339+sukarn-m@users.noreply.github.com>
2024-08-08 20:12:11 +0530

    modified:   files/scripts/ublue-rpms.sh

--- files/scripts/ublue-rpms.sh
+++ files/scripts/ublue-rpms.sh
@@ -4,12 +4,16 @@
 
 set -oue pipefail
 
-tree /tmp/
-exit 1
+# To get the list of packages available in a workflow log:
+# tree /tmp && exit 1
 
-# rpm-ostree install /tmp/config-rpms/*.noarch.rpm
-rpm-ostree install \
-/tmp/config-rpms/ublue-os-just*.noarch.rpm \
-/tmp/config-rpms/ublue-os-luks*.noarch.rpm \
-/tmp/config-rpms/ublue-os-udev-rules*.noarch.rpm \
-/tmp/config-rpms/ublue-os-update-services*.noarch.rpm
+rpm-ostree install /tmp/config-rpms/*.noarch.rpm
+
+# Alternatively, to skip installation of ublue-os-signing.noarch.rpm:
+# rpm-ostree install \
+# /tmp/config-rpms/ublue-os-just.noarch.rpm \
+# /tmp/config-rpms/ublue-os-luks.noarch.rpm \
+# /tmp/config-rpms/ublue-os-udev-rules.noarch.rpm \
+# /tmp/config-rpms/ublue-os-update-services.noarch.rpm
+
+rpm-ostree install /tmp/kernel-rpms/*.rpm
