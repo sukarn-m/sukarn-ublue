07573a23d01b3b249de3231eff9acdf4993990a5
sukarn-m <10946339+sukarn-m@users.noreply.github.com>
2024-08-09 11:42:12 +0530

    modified:   image_name-s.sh

--- files/scripts/image_name-s.sh
+++ files/scripts/image_name-s.sh
@@ -2,4 +2,4 @@
 
 IMAGE_DATE=$(date +%Y%m%d.%H)
 MAJOR_RELEASE_VERSION=$(grep -oP '[0-9]*' /etc/fedora-release)
-sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"Fedora Linux ${MAJOR_RELEASE_VERSION}.${IMAGE_DATE} \(Sukarn\)\"," /usr/lib/os-release
+sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"Sukarn OS \(based on Fedora Linux\) ${MAJOR_RELEASE_VERSION}.${IMAGE_DATE} \"," /usr/lib/os-release
