165a3b2ea58ab9cbd014bb15e8583ba75254be4a
Sukarn <10946339+sukarn-m@users.noreply.github.com>
2024-08-12 19:29:15 +0530

    Update nextcloud-vfs.sh

--- files/scripts/nextcloud-vfs.sh
+++ files/scripts/nextcloud-vfs.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-# On Fedora, nextcloud-client does not contain the libraries for virtual files support. Flatpak does not work with virtual files. AppImage does not integrate with Nautilus.
+# On Fedora, nextcloud-client does not contain the libraries for virtual files support. Flatpak and AppImage do not integrate with Nautilus.
 
 echo "Linking nextcloud libraries to enable virtual files support."
 ln -s -v /usr/lib64/nextcloudsync_vfs_suffix.so /usr/lib64/qt5/plugins/nextcloudsync_vfs_suffix.so
