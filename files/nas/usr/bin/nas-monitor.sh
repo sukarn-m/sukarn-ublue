060d464c30c0b400ad5735ce9d426d0d56c380c2
Sukarn <10946339+sukarn-m@users.noreply.github.com>
2024-08-21 16:03:59 +0530

    modified:   nas/usr/bin/nas-monitor.sh

--- files/nas/usr/bin/nas-monitor.sh
+++ files/nas/usr/bin/nas-monitor.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+function systemctl_start () {
+  if [ ! $(systemctl is-active --quiet ${1} ]; then
+    systemctl start ${1}
+  fi
+}
+
+function systemctl_stop () {
+  if [ $(systemctl is-active --quiet ${1} ]; then
+    systemctl stop ${1}
+  fi
+}
+
 if ping -c 1 truenas.home &> /dev/null
 then
-	systemctl start var-mnt-nas.automount
-	systemctl start var-mnt-roms.automount
+  systemctl_start var-mnt-nas.automount
+	systemctl_start var-mnt-roms.automount
 else
-	systemctl stop var-mnt-nas.automount
-	systemctl stop var-mnt-nas.mount
-	systemctl stop var-mnt-roms.automount
-	systemctl stop var-mnt-roms.mount
+	systemctl_stop var-mnt-nas.automount
+	systemctl_stop var-mnt-nas.mount
+	systemctl_stop var-mnt-roms.automount
+	systemctl_stop var-mnt-roms.mount
 fi
