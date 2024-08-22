ffce4bdefe5e87406d9ce7a8bd422cb11f4f3bf6
Sukarn <10946339+sukarn-m@users.noreply.github.com>
2024-08-22 19:38:16 +0530

    modified:   nvidia.sh

--- files/scripts/nvidia.sh
+++ files/scripts/nvidia.sh
@@ -9,9 +9,9 @@ chmod 0700 /usr/bin/nvidia-kargs
 echo "## Required for use of OBS-VkCapture in X11 environments" >> /etc/environment
 echo "## Nvidia users must additionally have nvidia-drm.modeset=1 in their kargs" >> /etc/environment
 echo "# OBS_USE_EGL=1" >> /etc/environment
-echo ""
+echo "" >> /etc/environment
 echo "## Required for GNOME VRR MR" >> /etc/environment
-echo "MUTTER_DEBUG_FORCE_KMS_MODE=simple" >> /etc/environment
+echo "# MUTTER_DEBUG_FORCE_KMS_MODE=simple" >> /etc/environment
 echo "" >> /etc/environment
 echo "## Gnome flickering / tearing fix" >> /etc/environment
 echo "# MUTTER_DEBUG_FORCE_EGL_STREAM=1" >> /etc/environment
