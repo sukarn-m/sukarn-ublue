b30403fcce98386c7e323efcd18c217ecbe8f49c
Sukarn <10946339+sukarn-m@users.noreply.github.com>
2024-08-23 16:08:42 +0530

    deleted:    files/scripts/tailscale.sh modified:   recipes/base-fonts.yml modified:   recipes/base-gnome.yml

--- files/scripts/tailscale.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo -P /etc/yum.repos.d/
-rpm-ostree install tailscale
