437d01a45bf14646f49e49668affc27c2aa580c2
sukarn-m <10946339+sukarn-m@users.noreply.github.com>
2025-01-23 14:20:54 +0530

    Fix Nextcloud VFS plugin symlinks because of shift from qt5 to qt6.

--- files/scripts/nextcloud-vfs.sh
+++ files/scripts/nextcloud-vfs.sh
@@ -6,5 +6,5 @@ set -oue pipefail
 # On Fedora, nextcloud-client does not contain the libraries for virtual files support. Flatpak and AppImage do not integrate with Nautilus.
 
 echo "Linking nextcloud libraries to enable virtual files support."
-ln -s -v /usr/lib64/nextcloudsync_vfs_suffix.so /usr/lib64/qt5/plugins/nextcloudsync_vfs_suffix.so
-ln -s -v /usr/lib64/nextcloudsync_vfs_xattr.so /usr/lib64/qt5/plugins/nextcloudsync_vfs_xattr.so
+ln -s -v /usr/lib64/nextcloudsync_vfs_suffix.so /usr/lib64/qt6/plugins/nextcloudsync_vfs_suffix.so
+ln -s -v /usr/lib64/nextcloudsync_vfs_xattr.so /usr/lib64/qt6/plugins/nextcloudsync_vfs_xattr.so
